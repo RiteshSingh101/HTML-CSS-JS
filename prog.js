823700295ef819804a81e4179ada6d64835cf3d1
Ritesh <riteshsaud@gmail.com>
2026-06-20 23:05:22 +0530

    save

--- prog.js
+++ prog.js
@@ -1,5 +1,19 @@
-add()
-
-function add(){
-    console.log("hello")
+function calculator(operation){
+    console.log("Calculating");
+    console.log(operation)
+    console.log(operation(4,2))
+    return "Sucessfully performed the operations"
 }
+
+function add(num1, num2){
+    console.log("Addition:-")
+    return num1 + num2;
+}
+
+function sub(num1,num2){
+    console.log("Substract: ")
+    return num1 - num2;
+}
+
+console.log(calculator(add));
+console.log(calculator(sub))
