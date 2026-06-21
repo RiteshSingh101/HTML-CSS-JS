5097b953c525064f47a38cbd3b0021b0ef655723
Ritesh <riteshsaud@gmail.com>
2026-06-22 00:53:37 +0530

    save

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.log(add())
 
-function add(){
-   console.log("hello ritesh")
-}
+// let fun = function show(){
+//     console.log("Say Hello!")
+//     return "India"
+// }
+// console.log(fun)
+// console.log(fun())
+
+// let a = () => { return "Jai Hind"}
+
+// console.log(a)
+// console.log(a())
+
+// let con = a => console.log("Hell",a)
+
+// console.log(con())
+
